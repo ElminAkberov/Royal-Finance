941cf7e2363965389fab927b9fb6f458d6368afb
ElminAkberov <ekberovelmin04@gmail.com>
2024-10-28 00:27:47 +0400

    C

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react';
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://dev.royal-pay.org',
+        target: 'https://dev.royal-pay.org/',
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1/internal') 
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1/internal/') 
       }
     }
   }
