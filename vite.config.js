61d29be562c692fd1027e54854a74bb7f64a95d9
ElminAkberov <ekberovelmin04@gmail.com>
2024-10-28 00:10:44 +0400

    RR

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dev.royal-pay.org/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1/internal/') 
-      }
-    }
-  }
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://dev.royal-pay.org', // API URL
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''), // isteği '/api' ile başlayacak şekilde yönlendirin
+            },
+        },
+    },
 });
