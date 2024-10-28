79f53213441de2304c984468e941d712828865a8
ElminAkberov <ekberovelmin04@gmail.com>
2024-10-28 10:54:01 +0400

    RF

--- src/api/refills.js
+++ src/api/refills.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
     const response = await fetch("https://dev.royal-pay.org/api/v1/internal/refills/", {
         method: req.method,
         headers: {
-            "Content-Type": "application/json",
             "Authorization": req.headers.authorization,
         },
     });
